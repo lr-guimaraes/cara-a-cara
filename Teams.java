e32b14da0c1a8b9941cae215c6c5eada84e2debe
lr-guimaraes <leandrorg307@gmail.com>
2022-06-06 01:13:28 -0300

    first update

---
+++ Teams.java
@@ -0,0 +1,14 @@
+package Teams;
+
+import java.util.ArrayList;
+
+public class Team{
+    private ArrayList<Team> alTeams;
+
+    public ListTeam(){
+        super();
+        this.alTeams = new ArrayList<Team>();
+    }
+
+    pub
+}
--- main.java
+++ main.java
@@ -10,6 +10,27 @@ public class Main {
 	private static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args){
+        //Teams per group
+        String[32][8]teams = {{"MANCHERSTER UNITED","FC BASEL","CSKA MOSKVA","BENFICA"},
+                              {"PARIS SAINT GERMAIN","BAYERN MUNCHEN","CELTIC","ANDERLECHT"},
+                              {"ROMA","CHELSEA","ATLETICO MADRID","QARABAG"},
+                              {"BARCELONA","JUVENTUS","SPORTING","OLYMPIACOS"},
+                              {"LIVERPOOL","SERVILLA","SPARTAK MOSKVA","MARIBOR"},
+                              {"MANCHERSTER CITY","SHAKTAR DONETSK","NAPOLI","FEYENOORD"},
+                              {"BASIKTAS","PORTO","RB LEIPZIG","MONACO"},
+                              {"TOTTENNHAM","REAL MADRID","BORUSSIA DORTMUND","APOEL"}};
         
+        String[]countries = {"INGLATERRA","SUIÇA","RUSSIA","PORTUGAL","FRANÇA","ALEMANHA","ESCOCIA",
+                            "BELGICA","ITALIA","ESPANHA","AZERBAJAO","GRACIA","ESLOVENIA","UCRANIA",
+                            "HOLANDA","TURQUIA","CHIPRE"};
+        //Played which servilla, Spartak Moskva, marobor, porto, manchester ciy, roma, real madrid
+        //campeao
+        //grupo E
+        //jogou na: inglaterra, espanha, russia, eslovenia, portugal e italia
+        //final com real madrid
+            
+
+
+
     }
 }
